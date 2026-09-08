18fc1fdedd383b1f0098342a03acce321e32b56a
P516n <pdg1013sec@gmail.com>
2026-09-08 14:48:03 +0900

    feat: Create config.js, add profile

---
+++ js/config.js
@@ -0,0 +1,24 @@
+const CONFIG = {
+  // 개인 프로필 기본 정보
+  GITHUB_USERNAME: 'P516n',
+  PROFILE: {
+    name: '박동규',
+    title: 'Security Specialist & Aspiring Web Developer',
+    typingTexts: [
+      'Security Specialist.',
+      'Aspiring Web Developer.',
+      'Creative Problem Solver.',
+      'Continuous Learner.'
+    ],
+    email: 'pdg1013sec@gmail.com',
+    githubUrl: 'https://github.com/P516n',
+    linkedinUrl: 'https://linkedin.com/in/%EB%8F%99%EA%B7%9C-%EB%B0%95-8794392ab'
+  },
+
+  // 인터랙션 임계값 설정
+  SCROLL: {
+    NAVBAR_THRESHOLD: 60,   // 네비게이션 스타일 변경 스크롤(px)
+    TOP_BTN_THRESHOLD: 300, // 탑 버튼 등장 스크롤(px)
+    INTERSECTION_THRESHOLD: 0.2 // 스크롤 애니메이션 임계값
+  }
+};
